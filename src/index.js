fd8035e9d70d2b5eec5159944797dc56e0a5c0b7
nexvoidai <nexvoidai@users.noreply.github.com>
2026-07-20 17:15:24 -0500

    Run deterministic paper portfolio cycles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 export { canonicalize, hash } from "./canonical.js";
 export { evaluate } from "./evaluate.js";
 export { normalizeIntent } from "./intent.js";
+export { runPaperCycle } from "./paper.js";
+export { applyPaperFill, buildRebalanceIntents } from "./portfolio.js";
 export {
   DurablePermitLedger,
   PermitLedger,
